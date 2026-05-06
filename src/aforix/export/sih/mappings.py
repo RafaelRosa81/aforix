dc77a588babeb6f816b52619712c95fd89aa5a8a
RafaelRosa81 <120938389+RafaelRosa81@users.noreply.github.com>
2026-05-05 23:20:28 -0300

    Add SIH mappings stub

---
+++ src/aforix/export/sih/mappings.py
@@ -0,0 +1,10 @@
+"""Mapping logic for SIH export.
+
+Future responsibilities:
+- station_id -> SIH id_estacion
+- instrument -> id_instrumento
+- instrument -> id_tipo_aforo
+- instrument -> id_instrumentos_rangos
+- datetime formatting
+- configurable source field resolution
+"""
